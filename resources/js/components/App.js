887746b62cefe0115bddfeebd033aa16c5db0eb3
brieucfresnel <fresnel.brieuc@gmail.com>
2020-05-19 17:21:50 +0200

    make app, home react components + link app component to laravel view

---
+++ resources/js/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function App() {
+    return (
+        <div className="wrapper">
+            Screen d'accueil
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ resources/js/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="wrapper">
+            Screen d'accueil
+        </div>
+    )
+}
+
+export default Home;
+
