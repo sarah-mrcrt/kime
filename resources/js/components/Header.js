fe073dddaa147c9da2a4e3956dd23c471367c7e6
brieucfresnel <fresnel.brieuc@gmail.com>
2020-05-27 15:33:07 +0200

    Fix Header.js conflict

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -29,15 +29,8 @@ function Header(props) {
                     }
                 </div>
             }
-<<<<<<< HEAD
-            <div className="page-header__text">
-                <h1 className="page-header__title">{props.title}</h1>
-                <div className="page-header__subtitle">{props.subtitle}</div>
-            </div>
-            <div className="page-header__background">
-=======
+
             <div className={"page-header__background " + align}>
->>>>>>> b9f28b8a2b180e07553933d13944c407996c106a
                 <img className="page-header__img" src={props.imageUrl}/>
                 <RoundBackground color={props.color} />
             </div>
