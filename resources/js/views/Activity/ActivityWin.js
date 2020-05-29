ee529b17678874b3778b40444776837737ef9766
AmelieCHAMBON <am.chambon@hotmail.com>
2020-05-29 14:15:35 +0200

    SASS/JS ActivityUpload

--- resources/js/views/Activity/ActivityWin.js
+++ resources/js/views/Activity/ActivityWin.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
-function ActivityWin(props) {
+function ActivityUpload(props) {
+
+    // récupérer l'input type="file" name="upload-file"
 
     return (
 
-        <div className="container yellow background">
-            <div className="container__body none">
-                <div className="content">
-                    ActivityWin
-                </div>
+        <div className="container yellow background activity-upload">
+        <a className="container__close" href="/">X</a>
+            <div className="content">
+                <h1>Ajouter ma création</h1>
             </div>
+            <label for="file-input" className="activity-upload__input-img">
+                <img src="/img/upload-img.svg"/>
+            </label>
+
+            <input id="file-input" className="activity-upload__input-file" name="upload-file" type="file"/>
         </div>
     ) 
 }
 
-export default ActivityWin;
+export default ActivityUpload;
