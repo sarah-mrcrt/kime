fdad4adacf93a5fddcb38396646b5f8e54516b69
AmelieCHAMBON <am.chambon@hotmail.com>
2020-05-30 12:39:04 +0200

    Mise en place de Font Awesome + Création du composant HeaderProfile.js

--- resources/js/views/Profil/Profil.js
+++ resources/js/views/Profil/Profil.js
@@ -2,8 +2,15 @@ import React, {useState, useContext} from 'react';
 import axios from 'axios';
 import { AuthDataContext } from '../../components/AuthDataProvider';
 import Navigation from '../../components/Navigation';
+import HeaderProfile from '../../components/HeaderProfile';
 
 function Profil(props) {
+
+    let kid = {id:2, name:"Jérôme", avatar:"/img/avatar-07.svg"}
+    let nb_trophies = 3;
+    // nb_trophies = nombre de trophée débloqué
+    let nb_activities = 10;
+    // nb_activities = nombre d'activités réalisées
 
     const [redirect, setRedirect] = useState(false);
     const { onLogout } = useContext(AuthDataContext);
@@ -24,11 +31,11 @@ function Profil(props) {
     }
 
     return (
-        <div className="container background">
+        <div className="container yellow background">                        
+        <HeaderProfile kid={kid} nb_trophies={nb_trophies} nb_activities={nb_activities} logout={logout} />
             <div className="container__body">
                 <div className="content">
                         Hello from Profil
-                        <button className="btn-common noMargin btn-common__red__fill" onClick={logout}>Déconnexion</button>
                         <Navigation page="profile" />
                 </div>
             </div>
