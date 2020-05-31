427136309cdb09b1f7183ffe65410e08c2082cf1
AmelieCHAMBON <am.chambon@hotmail.com>
2020-05-31 18:57:01 +0200

    SASS/JS ParentProfil + Rectification HeaderParent.js

--- resources/js/components/HeaderParent.js
+++ resources/js/components/HeaderParent.js
@@ -5,31 +5,8 @@ import {Redirect} from 'react-router-dom';
 
 function HeaderParent(props) {
 
-    // Déconnection de la section Parent + Redirection Welcome.js
-
-    const [redirect, setRedirect] = useState(false);
-    const { onLogout } = useContext(AuthDataContext);
-
-    function logoutWelcome() {
-        /*
-        axios.post("/api/auth/logout",{})
-        .then(json => {
-            let authData = {};
-            sessionStorage.setItem('authData', JSON.stringify(authData));
-            console.log(json.data.message);
-
-        }).then(onLogout)
-        .catch(error => {
-            console.log(error);
-        });
-
-        setRedirect(true);
-        */
-    }
-
     return (
         <div className="page-header-parent-profil">
-            <a className="container__logout" onClick={logoutWelcome} href="/"><i className="fas fa-power-off"></i></a>
             <div className="page-header-parent-profil__background ">
                 <img className="page-header-parent-profil__img" src="/img/avatar-parent.svg"/>
             </div>
