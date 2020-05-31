65d83e2390c480c4f80ea5605ff8d69ff3de5f78
AmelieCHAMBON <am.chambon@hotmail.com>
2020-05-31 17:25:03 +0200

    Continuation de CreationsParent.js

--- resources/js/components/CreationsParent.js
+++ resources/js/components/CreationsParent.js
@@ -16,10 +16,57 @@ function CreationsParent(props) {
         {id: 2, kid_id:2, activity_id:3, img:"/img/category.jpg", created_at:'2020-05-30 11:08:13'}
     ]
     */
+
+    const convertirDate = (date) => {
+        let tmp = date.split(' ');
+        tmp = tmp[0].split('-');
+        switch(tmp[1]) {
+            case '01':
+                tmp[1] = "Janvier"
+                break;
+            case '02':
+                tmp[1] = "Février"
+                break;
+            case '03':
+                tmp[1] = "Mars"
+                break;
+            case '04':
+                tmp[1] = "Avril"
+                break;
+            case '05':
+                tmp[1] = "Mai"
+                break;
+            case '06':
+                tmp[1] = "Juin"
+                break;
+            case '07':
+                tmp[1] = "Juillet"
+                break;
+            case '08':
+                tmp[1] = "Août"
+                break;
+            case '09':
+                tmp[1] = "Septembre"
+                break;
+            case '10':
+                tmp[1] = "Octobre"
+                break;
+            case '11':
+                tmp[1] = "Novembre"
+                break;
+            case '12':
+                tmp[1] = "Décembre"
+                break;
+        }
+        return tmp[2] + " " + tmp[1] +" " +tmp[0]
+    }
+
     return (
         <div className="creations-parent">
 
             {props.creations.map((creation,index) => {
+
+                let date = convertirDate(creation.created_at);
 
                 // Récupérer l'activité de la dernière création
                 let activity = {id:3, name:"Grenouille",};
@@ -43,19 +90,22 @@ function CreationsParent(props) {
                     <div className="creations-parent__creation" key={index}>
 
                         <div className={"creations-parent__creation__body"}>
-                            <div className={"creations-parent__creation__body__img" + index}>
-                                <img src={creation.img} key={index} />
+                            <div className={"creations-kid__creation__imgs"}>
+                                <div className={"creations-parent__creation__body__imgs__img"}>
+                                    <img src={creation.img} key={index} />
+                                </div>
                             </div>
                             <div className="creations-parent__creation__body__infos">
                                 <img className="creations-parent__creation__body__infos__img" src={kid.avatar} />
                                 <p className="creations-parent__creation__body__infos__title">{kid.name}</p>
+                                <p className="creations-parent__creation__body__infos__subtitle">{date}</p>
                             </div>
                         </div>
 
                         <div className="creations-parent__creation__infos">
                             <p className="creations-parent__creation__infos__subtitle">{sub_category.name}</p>
                             <p className="creations-parent__creation__infos__title">{activity.name}</p>
-                            <span className="creations-parent__creation__infos__subtitle">Share</span>
+                            <span className="creations-parent__creation__infos__share">Share</span>
                         </div>
 
                     </div>
