4e46ec260b05ed5d68f81499f7f410b60bd2e25b
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2020-05-31 19:14:09 +0200

    search for an activity and it displays the creation + road crossing at second level (1.2)

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Activity;
+use App\Creation;
 use App\Kid;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,12 +12,15 @@ class SearchController extends Controller
 {
     public function search($parameter) {
         $kids = Kid::where('user_id', '=', Auth::id())->whereRaw("name LIKE CONCAT(?,'%')", [$parameter])->get();
-        
-        $activities = Activity::whereRaw("name LIKE CONCAT(?,'%')", [$parameter])->get();
+
+        // Afficher la création qui a pour nom l'activité XX
+        $creations = Creation::
+        join('activities', 'activities.id', '=', 'creations.activity_id')
+        ->whereRaw("name LIKE CONCAT(?,'%')", [$parameter])->get();
 
         return response()->json([
             'recherche' => $parameter,
-            'activity' => $activities,
+            'activity' => $creations,
             'kid' => $kids, 
         ], 200);
     }
