88d185820db973691789760b035a18da63cd72af
brieucfresnel <fresnel.brieuc@gmail.com>
2020-06-01 16:34:46 +0200

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{path?}', 'app');
+Route::view('/{path?}', 'app')->where('path', '.*');
 Route::view('/test/test', 'testpage');
 Auth::routes();
