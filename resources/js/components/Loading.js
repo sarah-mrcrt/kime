8ea710a0482a3bf6e3f937b9b7c5d63a86b21276
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2020-06-02 14:27:05 +0200

    test server  reconfig

--- resources/js/components/Loading.js
+++ resources/js/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Loading(props) {
     return (
         <div className="loading-container container red background">
-            <img src="kime-logo.svg" alt="Kime logo"></img>
+            <img src="/kime-logo.svg" alt="Kime logo"></img>
             <h1 className="logotypo">Kime</h1>
             <p>Chargement</p>
         </div>
